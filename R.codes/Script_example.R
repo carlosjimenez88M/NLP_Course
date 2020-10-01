f385d0f21d637705dd9246221c8724f52938792a
Daniel Jiménez <cjimenez187@aol.com>
2020-09-30 19:56:35 -0500

    Comparativa entre las cantantes

--- R.codes/Script_example.R
+++ R.codes/Script_example.R
@@ -103,3 +103,48 @@ taylor_swift_words %>%
   mutate(word = fct_reorder(word, n)) %>%
   ggplot(aes(n, word)) +
   geom_col()
+
+
+
+## Rama Beyonce
+ts <- taylor_swift_lyrics %>%
+  rename_all(str_to_lower) %>%
+  rename(song = title) %>%
+  select(-album)
+
+beyonce <- beyonce_lyrics %>%
+  select(artist = artist_name, song = song_name, lyrics = line)
+artist_song_words_raw <- bind_rows(ts, beyonce) %>%
+  unnest_tokens(word, lyrics) %>%
+  count(artist, song, word)
+
+artist_song_words <- artist_song_words_raw %>%
+  anti_join(stop_words, by = "word")
+
+
+by_artist_word <- artist_song_words %>%
+  group_by(artist, word) %>%
+  summarize(num_songs = n(),
+            num_words = sum(n)) %>%
+  mutate(pct_words = num_words / sum(num_words)) %>%
+  group_by(word) %>%
+  mutate(num_words_total = sum(num_words)) %>%
+  ungroup()
+word_differences <- by_artist_word %>%
+  bind_log_odds(artist, word, num_words) %>%
+  arrange(desc(abs(log_odds_weighted))) %>%
+  filter(artist == "Beyoncé") %>%
+  slice_max(num_words_total, n = 100, with_ties = FALSE) %>%
+  slice_max(abs(log_odds_weighted), n = 25, with_ties = FALSE) %>%
+  mutate(word = fct_reorder(word, log_odds_weighted)) %>%
+  mutate(direction = ifelse(log_odds_weighted > 0, "Beyoncé", "Taylor Swift"))
+word_differences %>%
+  ggplot(aes(log_odds_weighted, word, fill = direction)) +
+  geom_col() +
+  scale_x_continuous(breaks = log(2 ^ seq(-6, 9, 3)),
+                     labels = paste0(2 ^ abs(seq(-6, 9, 3)), "X")) +
+  labs(x = "Relative use in Beyoncé vs Taylor Swift (weighted)",
+       y = "",
+       title = "Which words most distinguish Beyoncé and Taylor Swift songs?",
+       subtitle = "Among the 100 words most used by the artists (combined)",
+       fill = "")
